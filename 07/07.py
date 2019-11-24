70d1eb953e62596085dd9e5b0e98020a9b1da0f5
samtomcom <sam@samtom.xyz>
2019-11-24 00:18:09 +0000

    idk

--- 07/07.py
+++ 07/07.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def value(x) -> bool:
+def value(wires, x) -> bool:
     #print("  {}".format(repr(x)))
     try:
         int(x)
@@ -10,45 +10,45 @@ def value(x) -> bool:
             return np.uint16(0)
         return wires[x]
 
+def scan(wires, lines):
+    for l in lines:
+        #print(repr(l))
+
+        operation, w = l.split(" -> ")
+        os = operation.split(" ")
+
+        w = w[:-1]
+        wires[w] = value(wires, w)
+
+        if len(os) == 1:
+            wires[w] = value(wires, os[0])
+    
+        elif len(os) == 2:
+            wires[w] = ~ value(wires, os[1])
+
+        else:
+            lhs = value(wires, os[0])
+            rhs = value(wires, os[2])
+
+            if os[1] == "LSHIFT":
+                wires[w] = lhs << rhs
+            elif os[1] == "RSHIFT":
+                wires[w] = lhs >> rhs
+            elif os[1] == "AND":
+                wires[w] = lhs & rhs
+            elif os[1] == "OR":
+                wires[w] = lhs | rhs
+
+    return wires
+        
+
 with open("input.txt", "r") as f:
-    lines = f.readlines()
+    ls = f.readlines()
 
-wires = dict()
+ws = dict()
 
-for l in lines:
-    #print(repr(l))
+ws = scan(ws, ls)
+ws = scan(ws, ls)
 
-    operation, w = l.split(" -> ")
-    os = operation.split(" ")
-
-    w = w[:-1]
-    wires[w] = value(w)
-
-    if len(os) == 1:
-        wires[w] = value(os[0])
-   
-    elif len(os) == 2:
-        wires[w] = ~ value(os[1])
-
-    else:
-        lhs = value(os[0])
-        rhs = value(os[2])
-
-        if os[1] == "LSHIFT":
-            wires[w] = lhs << rhs
-        elif os[1] == "RSHIFT":
-            wires[w] = lhs >> rhs
-        elif os[1] == "AND":
-            wires[w] = lhs & rhs
-        elif os[1] == "OR":
-            wires[w] = lhs | rhs
-
-    #print(wires)
-        
-print(wires["a"])
-
-#print()
-
-#print(wires)
-        
+print(ws["a"])
 
